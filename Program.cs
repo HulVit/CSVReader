8a225ce82addec066e481a2921f3e84152e58692
Vitali Hulevich <hulvital@gmail.com>
2022-10-13 20:26:11 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("-  - --> Welcome to CSVReader version 0.0.1 <-- -  -");
+
+string filename = "/home/user/Projects/CSVReader/employees.csv";
+
+using (StreamReader reader = new StreamReader(filename))
+{
+    string? line;
+
+    while ((line = reader.ReadLine()) != null) {
+        System.Console.WriteLine(line);
+    }
+}
